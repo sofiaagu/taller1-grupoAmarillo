9050641a85ebc7a93eebd7300915e5b51b75ce7c
JocManueh <Jose_manuel.diaz@uao.edu.co>
2025-09-01 21:21:52 -0500

    Cambio en cliente
    Cambio en cliente por ambiguedades y errores en el codigo

--- Taller 1/Assets/Scripts/Cliente.cs
+++ Taller 1/Assets/Scripts/Cliente.cs
@@ -1,28 +1,26 @@
 using UnityEngine;
 
-public class Cliente : Persona
+public class ClienteNuevo : Persona
 {
-    public string idCliente { get; set; }
-    public string tramite { get; set; } 
-    public float tiempoAtencion { get; set; }
+    public string IdCliente { get; set; }
+    public string Tramite { get; set; } 
+    public float TiempoAtencion { get; set; }
 
     private static int contadorID = 0;
 
-    public Cliente(string nombre, string correo, string direccion, string tramite, float tiempoAtencion)
+    public ClienteNuevo(string nombre, string correo, string direccion, string tramite, float tiempoAtencion)
         : base(nombre, correo, direccion)
     {
         contadorID++;
-        this.idCliente = "C" + contadorID; 
-        this.tramite = tramite;
-        this.tiempoAtencion = tiempoAtencion;
+        this.IdCliente = $"C{contadorID}";
+        this.Tramite = tramite;
+        this.TiempoAtencion = tiempoAtencion;
     }
 
     public override string ToString()
     {
-        return $"[{idCliente}] {nombre} - {tramite} (Tiempo: {tiempoAtencion}s)";
+        return $"[{IdCliente}] {nombre} - {Tramite} (Tiempo: {TiempoAtencion}s)";
     }
 
-    public  int GenerarTiempoAtencion;
-    
-
+    public int GenerarTiempoAtencion;
 }
