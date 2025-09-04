b2e09cd2b88f38c9d22d7bd2dc9763c066803c6a
JocManueh <Jose_manuel.diaz@uao.edu.co>
2025-09-03 21:36:47 -0500

    Mejora de interfaz
    Mejora by jocsito

---
+++ Taller 1/Assets/Scripts/Audio.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class Audio : MonoBehaviour
+{
+    [Header("referencias")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Button SiguienteAudio;
+
+    [SerializeField] List<AudioClip> listClips = new List<AudioClip>();
+
+    [SerializeField] bool loopPlaylist = true;
+
+    private int index = -1;
+
+    private void Awake()
+    {
+        if (SiguienteAudio != null)
+        {
+            SiguienteAudio.onClick.AddListener(PlayNext);
+        }
+    }
+    private void OnDestroy()
+    {
+        if (SiguienteAudio != null)
+        {
+            SiguienteAudio.onClick.RemoveListener(PlayNext);
+        }
+    }
+    public void PlayNext()
+    {
+        if (listClips == null || listClips.Count == 0 || audioSource == null) return;
+
+        index++;
+
+        if (index >= listClips.Count)
+        {
+            if (loopPlaylist)
+            {
+                index = 0;
+            }
+            else
+            {
+                index = listClips.Count - 1;
+            }
+        }
+
+        audioSource.Stop();
+        audioSource.clip = listClips[index];
+        audioSource.Play();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
